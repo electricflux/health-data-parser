f51f435b8132ab30f3caae8784ca75f2e3263450
Sid <electricflux@gmail.com>
2018-05-09 19:47:13 -0700

    Temp commit

--- parse_health_data.rb
+++ parse_health_data.rb
@@ -1,6 +1,7 @@
 require 'pdf-reader'
 require 'json'
 require_relative './parsers/walk_in_lab'
+require_relative './parsers/kaiser'
 
 WALK_IN_LAB_DIR = '/data/walk_in_lab'
 files = Dir.entries(File.dirname(__FILE__)+WALK_IN_LAB_DIR)
@@ -11,4 +12,17 @@ files.each do |file|
   results_list.concat(Parser::WalkInLab.parse(File.dirname(__FILE__)+WALK_IN_LAB_DIR+'/'+file))
 end
 
-puts JSON.pretty_generate(results_list)
+KAISER_DIR = '/data/kaiser'
+files = Dir.entries(File.dirname(__FILE__)+KAISER_DIR)
+results_list = []
+files.each do |file|
+  next unless file.end_with?('pdf')
+  results_list.concat(Parser::Kaiser.parse(File.dirname(__FILE__)+KAISER_DIR+'/'+file))
+end
+
+outfile = File.new("./output/json/results.json", File::CREAT|File::TRUNC|File::RDWR, 0777)
+output = JSON.pretty_generate(results_list)
+output.delete! '\\'
+output.gsub!("\"{","{")
+output.gsub!("}\"","}")
+outfile << output
